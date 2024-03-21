a7790079b98422d2a67215ab90decc528b33b9e4
HULIGE <kababanda@gmail.com>
2024-03-21 22:45:04 +0700

    nhay

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -5,56 +5,59 @@ namespace Player
 {
     public class PlayerMovement : MonoBehaviour
     {
-        [SerializeField ] private float speed;
-        private Rigidbody2D body;
+        [SerializeField] private float speed;
+        private Rigidbody2D _body;
         public Animator anim;
 
-        private bool flipplayer = true;
+        private bool _flipplayer = true;
 
-        public bool isJumping = false;
-        
+        public bool isJumping;
+
+        public Transform groundcheck;
+
+        public LayerMask groundLayer;
+
+        public bool isGrounded;
+
 
         private void Awake()
         {
             // grab references for rigidbody and animator form object
-            body = GetComponent<Rigidbody2D>();
+            _body = GetComponent<Rigidbody2D>();
             anim = GetComponent<Animator>();
         }
 
         private void Update()
         {
-            float horizontalInput = Input.GetAxis("Horizontal");
-            body.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.velocity.y);
-        // flip player when moving left-right
-        if (horizontalInput > 0 && !flipplayer)
-        {
-            flip();
-        }
-        else if (horizontalInput < 0 && flipplayer)
-        {
-            flip();
-        }
-        
-        
-        
-        //jump
-            if (Input.GetKey(KeyCode.Space))
-                body.velocity = new Vector2(body.velocity.x, speed);
+
+            Debug.Log(isGrounded);
+            var horizontalInput = Input.GetAxis("Horizontal");
+            _body.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, _body.velocity.y);
+            // flip player when moving left-right
+            if (horizontalInput > 0 && !_flipplayer)
+                Flip();
+            else if (horizontalInput < 0 && _flipplayer) Flip();
+
+            isGrounded = Physics2D.OverlapCapsule(groundcheck.position, new Vector2(0.18f, 0.03f),
+                CapsuleDirection2D.Horizontal, 0, groundLayer);
+
+            //jump
+            if (Input.GetKey(KeyCode.Space) && isGrounded)
+                _body.velocity = new Vector2(_body.velocity.x, 2);
             isJumping = false;
-            
-        //set the animation parameters  
+
+            //set the animation parameters  
             anim.SetFloat("walk", MathF.Abs(horizontalInput));
         }
+
         //flipplayer
-        void flip()
+        private void Flip()
         {
-            Vector3 currentScale = transform.localScale;
+            var currentScale = transform.localScale;
             currentScale.x *= -1;
             transform.localScale = currentScale;
 
-            flipplayer = !flipplayer;
+            _flipplayer = !_flipplayer;
         }
-        
-        
     }
 }
